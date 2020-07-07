86f471ba75d056bb945930da1a1f33f1869a26da
aryanp1408 <aryanpuri07@gmal.com>
2020-07-07 22:05:11 +0530

    c-29

--- Slingshot.js
+++ Slingshot.js
@@ -9,6 +9,9 @@ class SlingShot{
         this.pointB = pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
+        this.catapult1 = loadImage("sprites/sling1.png");
+        this.catapult2 = loadImage("sprites/sling2.png");
+        this.rubberBand1 = loadImage("sprites/sling3.png");   
     }
 
     fly(){
@@ -16,11 +19,26 @@ class SlingShot{
     }
 
     display(){
+        image(this.catapult1,200,30);
+        image(this.catapult2,172,30);
+        
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
+            if(pointA.x<200){
+
+            
             strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            line(pointA.x-25, pointA.y, pointB.x, pointB.y-30);
+            line(pointA.x-25, pointA.y, pointB.x+20,pointB.y-30);
+            image(this.rubberBand1,pointA.x-25,pointA.y-10, 15,30);
+
+            }
+            else{
+                 line(pointA.x+25, pointA.y, pointB.x, pointB.y-30);
+                line(pointA.x+25, pointA.y, pointB.x+20,pointB.y-30);
+                image(this.rubberBand1,pointA.x+25,pointA.y-10, 15,30);
+    }
         }
     }
     
--- sketch.js
+++ sketch.js
@@ -38,7 +38,7 @@ function setup(){
 
     bird = new Bird(100,100);
 
-    //log6 = new Log(230,180,80, PI/2);
+    log6 = new Log(230,180,80, PI/2);
     slingshot = new SlingShot(bird.body,{x:200, y:100});
 }
 
